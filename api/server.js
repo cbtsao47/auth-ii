f9d2ae395ffc3424ed8f3a19f04ccbf4c59be0a8
wisdomtooth <cbtsao47@gmail.com>
2019-01-23 15:18:24 -0600

    add migration

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const configMiddleware = require("../config/middleware");
 const server = express();
+const db = require("../data/dbConfig");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 configMiddleware(server);
 
